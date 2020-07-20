89d50106adbcf2775fe3974ee678d484a72c4618
Kumar Rishabh <kumarrishabh@sjsu.edu>
2020-07-20 16:41:36 -0700

    changed config

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ docker push rishu53/energy-frontend:latest
 docker push rishu53/energy-backend:latest
 docker push rishu53/energy-frontend:$GIT_SHA
 docker push rishu53/energy-backend:$GIT_SHA
-kubectl apply -f k8s
+kubectl apply -f simplek8s
 kubectl set image deployments/server-deployment server=rishu53/energy-backend:$GIT_SHA
 kubectl set image deployments/client-deployment frontend=rishu53/energy-frontend:$GIT_SHA
 
